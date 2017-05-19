34509503290a12748c437e07b1cf68fdfeeab096
PatrickWienzek <Patrick.Wienzek@informatik.hs-fulda.de>
2017-05-20 00:18:51 +0200

    SpawnMeteors
    prefab&Scripts

---
+++ BeansJam/Assets/Scripts/SpawnSphere.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSphere : MonoBehaviour {
+	
+	public GameObject pref;
+	public float waitTime = 2f;
+	private float timer = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		if (timer >= waitTime) {
+			Instantiate (pref, new Vector2 (10, Random.Range(-5, 5)), Quaternion.identity);
+			timer = timer % waitTime;
+		}
+	}
+}
