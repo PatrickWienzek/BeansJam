a764e2f82334836955685db73124dae5d7fa76d7
Heinki <Heinki@hotmail.de>
2017-05-20 22:43:15 +0200

    Supermann
    Easter Egg

---
+++ BeansJam/Assets/Scripts/Superman.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Superman : MonoBehaviour {
+
+    private GameObject _camera;
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Fly();
+	}
+
+    void Fly()
+    {
+
+    }
+}
