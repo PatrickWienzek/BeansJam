da0cc57da9de6f5e97511a4093ff501c8f739081
PatrickWienzek <Patrick.Wienzek@informatik.hs-fulda.de>
2017-05-20 20:16:32 +0200

    CurrAirstate
    Fly with no boundaries,
    meteor collision

--- BeansJam/Assets/Scripts/Moving_Ball.cs
+++ BeansJam/Assets/Scripts/Moving_Ball.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Moving_Ball : MonoBehaviour {
 
+	public ParticleSystem part;
+	private ParticleSystem.EmissionModule em;
 
 	private Rigidbody2D rb;
 	private float acc = 1.1f;
 	private float rotOverTime = 0f;
 
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
 		Physics2D.gravity = Vector2.zero;
+		part = GameObject.Find ("death_crush1").GetComponent<ParticleSystem>();
+		em = part.emission;
 	}
 	
 	// Update is called once per frame
@@ -22,5 +27,13 @@ public class Moving_Ball : MonoBehaviour {
 		rb.MoveRotation (50.0f * rotOverTime);
 	}
 
-
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		Debug.Log ("HI");
+		if (other.tag == "Player") {
+			em.enabled = true;
+			other.GetComponent<SpriteRenderer> ().enabled = false;
+			Destroy (other.gameObject, 0.2f);
+		}
+	}
 }
