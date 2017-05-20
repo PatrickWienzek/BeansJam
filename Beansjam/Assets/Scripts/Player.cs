e7828c8d90933bf662f614705b20dfdf6e0dad4d
Heinki <Heinki@hotmail.de>
2017-05-20 12:33:02 +0200

    Player & Planet
    Stuff

--- Beansjam/Assets/Scripts/Player.cs
+++ Beansjam/Assets/Scripts/Player.cs
@@ -7,32 +7,27 @@ public class Player : MonoBehaviour {
 
     public int live = 3;
     public float speed = 0.55f;
-    public float jumpForce = 50.0f;
+    public float jumpForce = 15.0f;
     private Rigidbody2D _rigidbody;
     private RotationPlanet _planet;
     private bool _jumpPossible = false;
+    private Transform _jumpForcePosition;
+    private Transform _planetCore;
+
 
 
 	// Use this for initialization
 	void Start () {
         _rigidbody = GetComponent<Rigidbody2D>();
         _planet = GameObject.FindGameObjectWithTag("Planet").GetComponent<RotationPlanet>();
-	}
+        _planetCore = GameObject.FindGameObjectWithTag("Planet").transform.GetChild(0);
+        _jumpForcePosition = transform.GetChild(1).transform;
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.GetKey(KeyCode.Space) && _jumpPossible)
-        {
-            _rigidbody.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
-            _planet.isJumping(true);
-
-            transform.Translate(Input.GetAxis("Horizontal") * speed,0,0);
-        }
-        else
-        {
-            _planet.isJumping(false);
-        }
+        Move();
 
         var nearestPlanet = (
             from planet in GameObject.FindGameObjectsWithTag("Planet")
@@ -44,7 +39,35 @@ public class Player : MonoBehaviour {
         foreach(var planet in GameObject.FindGameObjectsWithTag("Planet")) {
             planet.GetComponent<RotationPlanet>().enabled = planet == nearestPlanet;
         }
+        Gravity();
     }
+
+    private void Move()
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * speed, 0, 0);
+
+        if (Input.GetKey(KeyCode.Space) && _jumpPossible)
+        {
+            _rigidbody.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+            _planet.isJumping(true);
+        }
+        else
+        {
+            _planet.isJumping(false);
+        }
+    }
+
+    void Gravity()
+    {
+        if(!_jumpPossible)
+        {
+            //Gravity towards the Planetcore on the current planet
+            transform.position = Vector3.MoveTowards(transform.position, _planetCore.position, 0.2f);
+            transform.up = transform.position - _planetCore.position;
+        }
+
+    }
+
 
     private void OnCollisionStay2D(Collision2D collision)
     {
--- Beansjam/Assets/Scripts/RotationPlanet.cs
+++ Beansjam/Assets/Scripts/RotationPlanet.cs
@@ -15,10 +15,10 @@ public class RotationPlanet : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (!jump)
-        {
-            transform.Rotate(new Vector3(0, 0, Input.GetAxis("Horizontal") * rotationSpeed));
-        }
+        //if (!jump)
+        //{
+        //    transform.Rotate(new Vector3(0, 0, Input.GetAxis("Horizontal") * rotationSpeed));
+        //}
     }
 
     public void isJumping(bool jumped)
