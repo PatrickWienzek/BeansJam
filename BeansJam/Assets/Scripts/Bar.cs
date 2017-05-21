8271ea57f59422209780e67fc9f9283f4413aa22
PatrickWienzek <Patrick.Wienzek@informatik.hs-fulda.de>
2017-05-21 11:22:01 +0200

    Lifebar
    Lifebar Dummy

---
+++ BeansJam/Assets/Scripts/Bar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bar : MonoBehaviour {
+	
+	[SerializeField]
+	private float health;
+
+	[SerializeField]
+	private Image content;
+
+	public 	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		content.fillAmount = health;
+		health -= Time.deltaTime / 10;
+	}
+}
