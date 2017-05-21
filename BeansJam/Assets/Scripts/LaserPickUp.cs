d39a8d6f089016c917fdd88642372c80a75e3deb
Henrik Ilgen <Henrik.Ilgen@ActiveVB.de>
2017-05-21 20:55:34 +0200

    Moar Lasers

---
+++ BeansJam/Assets/Scripts/LaserPickUp.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserPickUp : MonoBehaviour {
+
+    public GameObject eduardLaser;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        var player = collision.gameObject;
+
+        var position = player.transform.position;
+        var rotation = player.transform.rotation;
+
+        player.tag = "Default";
+        player.GetComponentInChildren<SpriteRenderer>().enabled = false;
+        player.SetActive(false);
+        Destroy(player);
+
+        Instantiate(eduardLaser, position, rotation);
+
+        gameObject.SetActive(false);
+    }
+}
